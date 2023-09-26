a04abab6aef728443e46341c7dd7daa90d6edf90
yongh <42311029+mikuYongh@users.noreply.github.com>
2023-09-27 03:57:36 +0800

    优化Neko_NX.gd

--- addons/Neko_NX/Neko_NX.gd
+++ addons/Neko_NX/Neko_NX.gd
@@ -1,26 +1,37 @@
 @tool
 extends EditorPlugin
+var transparency = 0.65;#透明度
+var ambiguity = 6;#模糊度
+var brgn:NekoBackground;
 
-func _enter_tree():
+var baseNode;
+func _ready():
+	_load();
+func _load():
 	var edit=get_editor_interface();
-	var node=edit.get_base_control()
-	#print(node.get_children())
-	
-	var brgn:NekoBackground=preload("res://addons/Neko_NX/Background.tscn").instantiate();
+	baseNode=edit.get_base_control()
+	brgn=preload("res://addons/Neko_NX/Background.tscn").instantiate();
 	brgn.get_node("BlurY/SubViewport/BlurX/SubViewport/background").texture=preload("res://addons/Neko_NX/background.jpg")
-	node.get_parent().add_child(brgn);
-	node.get_parent().move_child(brgn,0)
+	brgn.get_node("BlurY").material.set_shader_parameter("radius", ambiguity)
+	brgn.get_node("BlurY/SubViewport/BlurX").material.set_shader_parameter("radius", ambiguity)
+	baseNode.get_parent().add_child(brgn);
+	baseNode.get_parent().move_child(brgn,0)
 	
-	node.modulate=Color(1,1,1,0.65)
-	
-	# Initialization of the plugin goes here.
+	baseNode.modulate=Color(1,1,1,transparency)
 	pass
-
-
 func _exit_tree():
-	# Clean-up of the plugin goes here.
+	
 	pass
-
+func _enable_plugin():
+	if !brgn:
+		_load()
+	
+func _disable_plugin():
+	if brgn&&baseNode:
+		baseNode.modulate=Color(1,1,1,1)
+		brgn.queue_free();
+	pass
+	
 func _process(delta):
 	
 		
